7d9b546e974dbcf9ef3e88d8c337e5b477ecb8f0
노지원 <bond9986@naver.com>
2021-12-03 06:13:45 +0900

    위치 수정

--- apollo.config.js
+++ apollo.config.js
@@ -1,8 +1,8 @@
 module.exports = {
     client: {
+        includes: ["./src/**/*.{tsx,ts}"],
+        tagName: "gql",
         service: {
-            includes: ["./src/**/*.{tsx,ts}"],
-            tagName: "gql",
             name: "instaclone-backend",
             url: "http://localhost:4000/graphql",
         }
